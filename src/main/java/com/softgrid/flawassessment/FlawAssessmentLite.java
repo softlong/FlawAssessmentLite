48150297352a8b5eb04c84532e773679b7287402
hecheng <cheryl.he@softgridinc.com>
2018-05-23 15:51:10 +0800

    insert 30582UI

--- src/main/java/com/softgrid/flawassessment/FlawAssessmentLite.java
+++ src/main/java/com/softgrid/flawassessment/FlawAssessmentLite.java
@@ -8,7 +8,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class FlawAssessment extends Application {
+public class FlawAssessmentLite extends Application {
 	
 	private DatabaseHandler databaseHandler;
 
